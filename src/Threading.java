9c5039fe2a4d176f9ba6f9da6731d53796088196
bonfotieno <bonnyotieno9@gmail.com>
2022-08-31 12:30:23 +0300

    updated

--- src/Threading.java
+++ src/Threading.java
@@ -1,28 +1,22 @@
-public class ThreadInterruptExample implements Runnable {
+public class Threading implements Runnable {
     public void run() {
         for (int i = 1; i <= 10; i++) {
             System.out.println("This is message #" + i);
-
             try {
                 Thread.sleep(2000);
-                continue;
-            } catch (InterruptedException ex) {
-                System.out.println("I'm resumed");
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
             }
         }
     }
 
     public static void main(String[] args) {
-        Thread t1 = new Thread(new ThreadInterruptExample());
+        Thread t1 = new Thread(new Threading());
         t1.start();
-
         try {
             Thread.sleep(5000);
-            t1.interrupt();
-
-        } catch (InterruptedException ex) {
-            // do nothing
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
         }
-
     }
 }
