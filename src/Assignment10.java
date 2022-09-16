6cb0e45a5e5ce822536590d3a26c3d538b17bdba
bonfotieno <bonnyotieno9@gmail.com>
2022-09-16 15:34:02 +0300

    updated

--- src/Assignment10.java
+++ src/Assignment10.java
@@ -1,7 +1,4 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.List;
+import java.util.*;
 
 public class Assignment10 {
     /**
@@ -15,18 +12,26 @@ public class Assignment10 {
      * allSwap(["ax", "bx", "cx", "cy", "by", "ay", "aaa", "azz"]) → ["ay", "by", "cy", "cx", "bx", "ax", "azz", "aaa"]
      * allSwap(["ax", "bx", "ay", "by", "ai", "aj", "bx", "by"]) → ["ay", "by", "ax", "bx", "aj", "ai", "by", "bx"]
      **/
-    public static String[] allSwap(String[] str){
-        for (int i = 0; i < str.length; i+=2) {
-
-                if (str[i].charAt(0) == str[i+1].charAt(0)) {
-                    String first = str[i];
-                    String second = str[i+1];
-                    str[i]=second;
-                    str[i+1]=first;
-                }
-
+    public static String[] allSwap(String[] strings){
+        String[] result=new String[strings.length];
+        Map<Character, Integer> map = new HashMap<Character, Integer>();
+        for (int i = 0; i < strings.length; i++) {
+            char c  = strings[i].charAt(0);
+            if (map.containsKey(c))
+            {
+                int p=map.get(c);
+                map.remove(c);
+                ;
+                result[i]=result[p];
+                result[p]=strings[i];
+            }
+            else
+            {
+                result[i]=strings[i];
+                map.put(c,i);
+            }
         }
-        return str;
+        return result;
     }
 
     public static void main(String[] args) {
